d38357fd90596e5ebb6c68e282d8a3a00a9ca276
Claudio Altamura <claudio@claudioaltamura.de>
2021-11-06 12:50:49 +0100

    new: first commit

---
+++ src/test/java/de/claudioaltamura/NativeSuperheroResourceIT.java
@@ -0,0 +1,9 @@
+package de.claudioaltamura;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSuperheroResourceIT extends SuperheroResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/de/claudioaltamura/SuperheroResourceTest.java
@@ -0,0 +1,21 @@
+package de.claudioaltamura;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class SuperheroResourceTest {
+
+    @Test
+    void testHelloEndpoint() {
+        given()
+          .when().get("/api/superheroes")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
